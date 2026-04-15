292652d122398d18563b460a0c47dc29e6160d37
weback1609 <bachlh03@gmail.com>
2026-04-15 14:25:05 +0700

    fix bug

--- web.frontend/app/_components/CodeMirrorEditor.tsx
+++ web.frontend/app/_components/CodeMirrorEditor.tsx
@@ -388,7 +388,7 @@ export default function CodeMirrorEditor({
             try {
               Vim.map(mapping.keys, mapping.action, mapping.mode);
             } catch (e) {
-              console.warn("Failed to map key:", mapping, e);
+              log.warn("Failed to map key:", mapping, e);
             }
           });
 
